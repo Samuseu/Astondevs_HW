764c55de3ec300216b39f3bb81c8af0cf84d132b
Vlad <s67.freelance@gmail.com>
2024-02-10 15:47:53 +0300

    add more options

---
+++ src/main/java/ru/astondevs/lesson2/moreoptions/Result.java
@@ -0,0 +1,42 @@
+package ru.astondevs.lesson2.moreoptions;
+
+
+public class Result {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign(1, 10);
+        printColor(20);
+        compareNumbers(20, 2);
+    }
+
+    private static void printThreeWords() {
+        String[] fruits = {"Orange", "Banana", "Apple"};
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+    }
+
+    private static void checkSumSign(int a, int b) {
+        int sum = a + b;
+        String result = switch (Integer.signum(sum)) {
+            case 1 -> "Сумма положительная";
+            default -> "Сумма отрицательная";
+        };
+        System.out.println(result);
+    }
+
+    private static void printColor(int value) {
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+
+    private static void compareNumbers(int a, int b) {
+        String result = (a >= b) ? "a>=b": "a<b";
+        System.out.println(result);
+    }
+}
