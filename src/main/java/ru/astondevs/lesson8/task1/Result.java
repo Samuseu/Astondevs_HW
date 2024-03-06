0d55a9deb80256097dafca04b7d5afdbbc9ea73a
Vlad <s67.freelance@gmail.com>
2024-03-07 00:34:39 +0300

    Update

--- src/main/java/ru/astondevs/lesson8/task1/Result.java
+++ src/main/java/ru/astondevs/lesson8/task1/Result.java
@@ -3,15 +3,34 @@ package ru.astondevs.lesson8.task1;
 import java.util.*;
 
 public class Result {
+
     public static void main(String[] args) {
-        String[] arrayWords = {"Максим","Влад","Анна","Лидия","Максим","Лидия","Влад","Антон","Паша"};
+        String[] words = {"Максим", "Влад", "Анна", "Лидия", "Максим", "Лидия", "Влад", "Антон", "Паша"};
 
-        Set<String> setWordsArray = new HashSet<>();
+        Map<String, Integer> wordCount = new HashMap<>();
 
-        for (String words: arrayWords) {
-            setWordsArray.add(words);
+        for (String word : words) {
+            int count = wordCount.getOrDefault(word, 0) + 1;
+            wordCount.put(word, count);
         }
 
+        Set<String> wordsUnique = new HashSet<>();
+        for (String word : wordCount.keySet()) {
+            if (wordCount.get(word) == 1) {
+                wordsUnique.add(word);
+            }
+        }
 
+        System.out.println("Уникальные слова:");
+        for (String word : wordsUnique) {
+            System.out.println(word);
+        }
+
+        System.out.println("\nКоличество повторений:");
+        for (Map.Entry<String, Integer> wordEntry : wordCount.entrySet()) {
+            String key = wordEntry.getKey();
+            int value = wordEntry.getValue();
+            System.out.printf("%s : %d\n", key, value);
+        }
     }
 }
