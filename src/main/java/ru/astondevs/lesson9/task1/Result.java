22e2efc774edeb87338106926bac74ece350ef3b
Vlad <s67.freelance@gmail.com>
2024-03-10 12:40:59 +0300

    Update

--- src/main/java/ru/astondevs/lesson9/task1/Result.java
+++ src/main/java/ru/astondevs/lesson9/task1/Result.java
@@ -1,6 +1,8 @@
 package ru.astondevs.lesson9.task1;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Result {
     public static void main(String[] args) {
@@ -9,8 +11,10 @@ public class Result {
             list.add((int) (Math.random() * 10));
         }
         int evenNumbers = (int) list.stream().filter(n -> n % 2 == 0).count();
+        List<Integer> evenNumbers1 = list.stream().filter(integer -> integer % 2 == 0).collect(Collectors.toList());
+        System.out.println("\nЧисла: " + list);
+        System.out.println("\nЧетные числа: " + evenNumbers1);
+        System.out.println("\nКоличество четных чисел: " + evenNumbers);
 
-        System.out.println("Числа: " + list);
-        System.out.println("Количество четных чисел: " + evenNumbers);
     }
 }
