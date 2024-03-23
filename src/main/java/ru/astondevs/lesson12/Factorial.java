d846a75c337b3835af12abae62c6fe41787ef1d6
Vlad Samuseu <s67.freelance@gmail.com>
2024-03-23 13:07:05 +0300

    update

--- src/main/java/ru/astondevs/lesson12/Factorial.java
+++ src/main/java/ru/astondevs/lesson12/Factorial.java
@@ -2,14 +2,15 @@ package ru.astondevs.lesson12;
 
 public class Factorial {
     public static int factorial(int num) {
+        if (num < 0) {
+            throw new IllegalArgumentException("Факториал не может быть отрицательным ");
+        }
         int factorial = 1;
-        if (num <= 0) {
-            System.out.println("Введите положительное число");
-        } else if (num > 0) {
-            for (int i = 1; i <= num; i++) {
-                factorial = factorial * i;
-            }
+        for (int i = 1; i <= num; i++) {
+            factorial = factorial * i;
         }
         return factorial;
     }
+
 }
+
--- src/test/java/ru/astondevs/lesson12/FactorialTests.java
+++ src/test/java/ru/astondevs/lesson12/FactorialTests.java
@@ -1,10 +1,49 @@
 package ru.astondevs.lesson12;
 
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class FactorialTests {
-    @Test
-    public void testFactorialnum(){
+    @DisplayName("Тестирование вычисления факториала для положительных чисел ")
+    @CsvSource({
+            "2,2",
+            "3,6",
+            "4,24",
+            "5,120",
+            "6,720",
+    })
+    @ParameterizedTest(name = "Факториал {0} должен быть равен {1}")
+    @Tag("UNIT_TEST")
+    public void testFactorialOfPositive(
+            int input,
+            int expected) {
+        Assertions.assertEquals(expected, Factorial.factorial(input));
 
+    }
+
+    @DisplayName("Тестирование вычисления факториала для нуля")
+    @Test
+    @Tag("UNIT_TEST")
+    public void testFactorialOfZero() {
+        Assertions.assertEquals(1, Factorial.factorial(0));
+    }
+
+    @DisplayName("Тестирование вычисления факториала для отрицательных чисел")
+    @ParameterizedTest(name = "Факториал от отрицательного числа {0} должен вызывать исключение")
+    @ValueSource(
+            ints = {-2,
+                    -3,
+                    -4,
+                    -5}
+    )
+    @Tag("UNIT_TEST")
+    public void testFactorialOfNegative(int input) {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> Factorial.factorial(input));
     }
 }
