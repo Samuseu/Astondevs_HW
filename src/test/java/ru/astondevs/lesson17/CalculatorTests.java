07b672932a107f1a2cc3025ad618cae45a5fb54c
Vlad Samuseu <s67.freelance@gmail.com>
2024-04-15 01:26:57 +0300

    update

--- src/test/java/ru/astondevs/lesson17/CalculatorTests.java
+++ src/test/java/ru/astondevs/lesson17/CalculatorTests.java
@@ -1,21 +1,31 @@
 package ru.astondevs.lesson17;
 
+import io.qameta.allure.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-
+@Epic("Калькулятор")
+@Feature("Базовые операции")
 public class CalculatorTests extends TestBase {
 
     @Test
     @Tag("Phone")
     @DisplayName("Тест сложения чисел")
+    @Description("Проверка операции сложения двух чисел")
+    @Story("Сложение")
+    @Owner("Влад Самусев")
     void numberAdditionTest() {
-        calculatorPage.clickDigitButton(2)
-                .performOperation("+")
-                .clickDigitButton(2)
-                .performOperation("=");
+        performNumberAddition();
+    }
+
+    @Step("Выполнение сложения чисел")
+    void performNumberAddition() {
+        calculatorPage.clickDigitButton(2);
+        calculatorPage.performOperation("+");
+        calculatorPage.clickDigitButton(2);
+        calculatorPage.performOperation("=");
         String result = calculatorPage.getResultText();
         Assertions.assertEquals("4", result, "Результат сложения неверен");
     }
@@ -23,37 +33,61 @@ public class CalculatorTests extends TestBase {
     @Test
     @Tag("Phone")
     @DisplayName("Тест вычитания чисел")
+    @Description("Проверка операции вычитания двух чисел")
+    @Story("Вычитание")
+    @Owner("Влад Самусев")
     void subTractionOfNumbersTest() {
-        calculatorPage.clickDigitButton(1)
-                .clickDigitButton(2)
-                .performOperation("-")
-                .clickDigitButton(6)
-                .performOperation("=");
+        performSubtractionOfNumbers();
+    }
+
+    @Step("Выполнение вычитания чисел")
+    void performSubtractionOfNumbers() {
+        calculatorPage.clickDigitButton(1);
+        calculatorPage.clickDigitButton(2);
+        calculatorPage.performOperation("-");
+        calculatorPage.clickDigitButton(6);
+        calculatorPage.performOperation("=");
         String result = calculatorPage.getResultText();
         Assertions.assertEquals("6", result, "Результат вычитания неверен");
     }
 
     @Test
     @Tag("Phone")
-    @DisplayName("Тест умножение чисел")
+    @DisplayName("Тест умножения чисел")
+    @Description("Проверка операции умножения двух чисел")
+    @Story("Умножение")
+    @Owner("Влад Самусев")
     void multiplicationOfNumbersTest() {
-        calculatorPage.clickDigitButton(3)
-                .performOperation("*")
-                .clickDigitButton(5)
-                .performOperation("=");
+        performMultiplicationOfNumbers();
+    }
+
+    @Step("Выполнение умножения чисел")
+    void performMultiplicationOfNumbers() {
+        calculatorPage.clickDigitButton(3);
+        calculatorPage.performOperation("*");
+        calculatorPage.clickDigitButton(5);
+        calculatorPage.performOperation("=");
         String result = calculatorPage.getResultText();
         Assertions.assertEquals("15", result, "Результат умножения неверен");
     }
 
     @Test
     @Tag("Phone")
-    @DisplayName("Тест деление чисел")
+    @DisplayName("Тест деления чисел")
+    @Description("Проверка операции деления двух чисел")
+    @Story("Деление")
+    @Owner("Влад Самусев")
     void divisionOfNumbersTest() {
-        calculatorPage.clickDigitButton(4)
-                .clickDigitButton(9)
-                .performOperation("/")
-                .clickDigitButton(7)
-                .performOperation("=");
+        performDivisionOfNumbers();
+    }
+
+    @Step("Выполнение деления чисел")
+    void performDivisionOfNumbers() {
+        calculatorPage.clickDigitButton(4);
+        calculatorPage.clickDigitButton(9);
+        calculatorPage.performOperation("/");
+        calculatorPage.clickDigitButton(7);
+        calculatorPage.performOperation("=");
         String result = calculatorPage.getResultText();
         Assertions.assertEquals("7", result, "Результат деления неверен");
     }
