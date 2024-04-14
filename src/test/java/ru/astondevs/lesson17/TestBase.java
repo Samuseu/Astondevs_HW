f38ccd5f0804dde43b6184190d69bb05111e178a
Vlad Samuseu <s67.freelance@gmail.com>
2024-04-14 23:41:13 +0300

    update

--- src/test/java/ru/astondevs/lesson17/TestBase.java
+++ src/test/java/ru/astondevs/lesson17/TestBase.java
@@ -20,7 +20,7 @@ public class TestBase {
         options.setAppPackage("com.google.android.calculator");
         options.setAppActivity("com.android.calculator2.Calculator");
         options.setNoReset(true);
-        driver = new AndroidDriver(new URL("http://localhost:4723/"), options);
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
         calculatorPage = new CalculatorPage(driver);
     }
 }
